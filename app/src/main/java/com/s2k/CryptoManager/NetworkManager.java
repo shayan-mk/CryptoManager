e7eb15979d58bf174054e79338c73c2ef1686535
Shayan Mohammadi <shayan.mkk@gmail.com>
2021-03-08 17:13:03 +0330

    Add NetworkManager dummy class

---
+++ app/src/main/java/com/s2k/CryptoManager/NetworkManager.java
@@ -0,0 +1,15 @@
+package com.s2k.CryptoManager;
+
+import com.s2k.CryptoManager.CryptoData;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+
+public class NetworkManager implements Callable<List<CryptoData>> {
+
+    @Override
+    public List<CryptoData> call() throws Exception {
+        //TODO: implement
+        return null;
+    }
+}
