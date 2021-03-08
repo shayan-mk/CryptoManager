53677ade344d59ccbfd1be77a69efea1ddf17045
ShayanTalaei <talaee.shayan@gmail.com>
2021-03-08 18:47:26 +0330

    OHCL class refactored, parameters and constructors

--- app/src/main/java/com/s2k/CryptoManager/NetworkManager.java
+++ app/src/main/java/com/s2k/CryptoManager/NetworkManager.java
@@ -1,9 +1,19 @@
 package com.s2k.CryptoManager;
 
+import android.util.Log;
+
 import com.s2k.CryptoManager.CryptoData;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.Callable;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class NetworkManager implements Callable<List<CryptoData>> {
 
@@ -21,9 +31,75 @@ public class NetworkManager implements Callable<List<CryptoData>> {
         return null;
     }
 
-    // what it should return?
-    public void getCryptoDataDialog(CryptoData cryptoData){
-        return;
+
+    public OHLC getCryptoDataDialog(CryptoData cryptoData){
+        return null;
+    }
+
+    public enum Range {
+        weekly,
+        oneMonth,
+    }
+
+    // برای دریافت کندل های روزانه به مدت یک هفته پارامتر دوم را "هفته ای" بدهید و
+    // برای دریافت کندل های روزانه به مدت یک ماه پارامتر دوم را "یک ماه" بدهید
+    // پارامتر اول هم نماد سکه مورد نظر خواهد بود
+
+    public void getCandles(String symbol,Range range) {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+        String miniUrl;
+        final String description;
+        switch (range) {
+
+            case weekly:
+                miniUrl = "period_id=1DAY".concat("&time_end=".concat(getCurrentDate()).concat("&limit=7"));
+                description = "Daily candles from now";
+                break;
+
+            case oneMonth:
+                miniUrl = "period_id=1DAY".concat("&time_end=".concat(getCurrentDate()).concat("&limit=30"));
+                description = "Daily candles from now";
+                break;
+
+            default:
+                miniUrl = "";
+                description = "";
+
+        }
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://rest.coinapi.io/v1/ohlcv/".concat(symbol).concat("/USD/history?".concat(miniUrl)))
+                .newBuilder();
+
+        String url = urlBuilder.build().toString();
+
+        // your coin IO API key...
+        final Request request = new Request.Builder().url(url)
+                .addHeader("X-CoinAPI-Key", "YOUR_COIN_IO_API_KEY")
+                .build();
+
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.v("TAG", e.getMessage());
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                } else {
+                    //extractCandlesFromResponse(response.body().string(), description);
+                }
+            }
+        });
+
+    }
+
+    private String getCurrentDate() {
+        return null;
     }
 
 }
--- app/src/main/java/com/s2k/CryptoManager/OHLC.java
+++ app/src/main/java/com/s2k/CryptoManager/OHLC.java
@@ -1,81 +1,22 @@
 package com.s2k.CryptoManager;
 
-import android.util.Log;
 
-import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.HttpUrl;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class OHLC {
 
-    public enum Range {
-        weekly,
-        oneMonth,
-    }
+    private float timeOpen;
+    private float timeClose;
+    private float priceOpen;
+    private float priceClose;
+    private float priceHigh;
+    private float priceLow;
 
-    // برای دریافت کندل های روزانه به مدت یک هفته پارامتر دوم را "هفته ای" بدهید و
-    // برای دریافت کندل های روزانه به مدت یک ماه پارامتر دوم را "یک ماه" بدهید
-    // پارامتر اول هم نماد سکه مورد نظر خواهد بود
-
-    public void getCandles(String symbol,Range range) {
-
-        OkHttpClient okHttpClient = new OkHttpClient();
-
-        String miniUrl;
-        final String description;
-        switch (range) {
-
-            case weekly:
-                miniUrl = "period_id=1DAY".concat("&time_end=".concat(getCurrentDate()).concat("&limit=7"));
-                description = "Daily candles from now";
-                break;
-
-            case oneMonth:
-                miniUrl = "period_id=1DAY".concat("&time_end=".concat(getCurrentDate()).concat("&limit=30"));
-                description = "Daily candles from now";
-                break;
-
-            default:
-                miniUrl = "";
-                description = "";
-
-        }
-
-        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://rest.coinapi.io/v1/ohlcv/".concat(symbol).concat("/USD/history?".concat(miniUrl)))
-                .newBuilder();
-
-        String url = urlBuilder.build().toString();
-
-        // your coin IO API key...
-        final Request request = new Request.Builder().url(url)
-                .addHeader("X-CoinAPI-Key", "YOUR_COIN_IO_API_KEY")
-                .build();
-
-        okHttpClient.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.v("TAG", e.getMessage());
-            }
-
-            @Override
-            public void onResponse(Call call, final Response response) throws IOException {
-
-                if (!response.isSuccessful()) {
-                    throw new IOException("Unexpected code " + response);
-                } else {
-                    //extractCandlesFromResponse(response.body().string(), description);
-                }
-            }
-        });
-
-    }
-
-    private String getCurrentDate() {
-        return null;
+    public OHLC(float timeOpen, float timeClose, float priceOpen, float priceClose, float priceHigh, float priceLow) {
+        this.timeOpen = timeOpen;
+        this.timeClose = timeClose;
+        this.priceOpen = priceOpen;
+        this.priceClose = priceClose;
+        this.priceHigh = priceHigh;
+        this.priceLow = priceLow;
     }
 }
