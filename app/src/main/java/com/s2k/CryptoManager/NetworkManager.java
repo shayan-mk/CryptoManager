02d8acb7ca122c27f79bf46352471c4e2824a4f3
ShayanTalaei <talaee.shayan@gmail.com>
2021-03-09 11:12:03 +0330

    NetworkManager: sign up, and set the apiKey

--- app/src/main/java/com/s2k/CryptoManager/NetworkManager.java
+++ app/src/main/java/com/s2k/CryptoManager/NetworkManager.java
@@ -22,6 +22,8 @@ import okhttp3.Response;
 
 public class NetworkManager implements Callable<List<CryptoData>> {
 
+    private static String apiKey = "b83b3e60-3bf0-41ed-b117-d38ec00b216d";
+
     @Override
     public List<CryptoData> call() throws Exception {
 
