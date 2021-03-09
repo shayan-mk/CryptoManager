ed2856dbbe984933b16cefe97822f2536e942436
ShayanTalaei <talaee.shayan@gmail.com>
2021-03-09 15:11:40 +0330

    CryptoData refactoring, adapted to the API format

--- app/src/main/java/com/s2k/CryptoManager/CryptoData.java
+++ app/src/main/java/com/s2k/CryptoManager/CryptoData.java
@@ -4,28 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CryptoData {
-    //TODO: symbol
     public String id;
     public String logo; //maybe need to change
     public String symbol;
     public String name;
-    public int oneHourChange;
-    public int oneDayChange;
-    public int oneWeekChange;
-    public int price;
+    public quote quote;
 
-    public CryptoData(String id,String iconUrl, String symbol, String name, int oneHourChange, int oneDayChange, int oneWeekChange, int price) {
-        this.id = id;
-        this.logo = iconUrl;
-        this.symbol = symbol;
-        this.name = name;
-        this.oneHourChange = oneHourChange;
-        this.oneDayChange = oneDayChange;
-        this.oneWeekChange = oneWeekChange;
-        this.price = price;
+    public class quote{
+        public quote() {
+        }
+
+        public class USD{
+            public USD(float price, float percent_change_1h, float percent_change_24h, float percent_change_7d) {
+                this.price = price;
+                this.percent_change_1h = percent_change_1h;
+                this.percent_change_24h = percent_change_24h;
+                this.percent_change_7d = percent_change_7d;
+            }
+
+            public float price;
+            public float percent_change_1h;
+            public float percent_change_24h;
+            public float percent_change_7d;
+
+        }
     }
 
-    // mock data
+    public CryptoData(String id, String logo, String symbol, String name, CryptoData.quote quote) {
+        this.id = id;
+        this.logo = logo;
+        this.symbol = symbol;
+        this.name = name;
+        this.quote = quote;
+    }
+
+
+
+    /*// mock data
     public static List<CryptoData> createCryptoDataList() {
         List<CryptoData> list = new ArrayList<>();
         CryptoData data1 = new CryptoData("0", "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png", "BTC", "Bitcoin", 0, -2, 2, 50000);
@@ -37,5 +52,5 @@ public class CryptoData {
             list.add(data2);
         }
         return list;
-    }
+    }*/
 }
